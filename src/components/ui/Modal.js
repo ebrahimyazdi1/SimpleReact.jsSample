0b31a9181672957db621afdca5c64a2410905a44
ebrahimyazdi1 <62615646+ebrahimyazdi1@users.noreply.github.com>
2023-01-24 19:33:52 +0330

    Update Modal.js

--- src/components/ui/Modal.js
+++ src/components/ui/Modal.js
@@ -1,13 +1,5 @@
 import classes from './Modal.module.css'
 function Modal(props){
-    function confirmHandler()
-    {
-        props.onConfirm();
-    }
-    function cancelHandler()
-    {
-        props.onCancel();
-    }
    return( <div className={classes.modal}>
         <p> Are you Sure?!</p>
         <button className={classes.btnalt} onClick={props.onConfirm}>yes</button>
